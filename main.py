9969fc57506733f700bb57a5c9209d2b884683ea
piyushrawat01763-crypto <piyushrawat01763@gmail.com>
2026-03-29 16:32:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+# ============================================
+# AQI PREDICTOR - CLEAN & SAFE VERSION
+# ============================================
 
+# Step 1: Import libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import joblib
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
